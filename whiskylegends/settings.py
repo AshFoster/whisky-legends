719bb7b76ab960eefcd3511c35df57fdb20a08ae
AshFoster <ashfoster87@gmail.com>
2022-03-29 17:05:46 +0000

    Add home app, views, urls and index.html

--- whiskylegends/settings.py
+++ whiskylegends/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 
     'storages',
 ]
@@ -65,7 +66,10 @@ ROOT_URLCONF = 'whiskylegends.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
