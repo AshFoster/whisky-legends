ad311f6a0ccc4a75b42a2e49a24e1f70b648642a
AshFoster <ashfoster87@gmail.com>
2022-04-05 16:18:31 +0000

    Customised shop app admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,8 +1,61 @@
 from django.contrib import admin
-from .models import Type, Brand, Country, Region, Product
+from .models import Type, Brand, Country, Region, Flavour, Product
 
-admin.site.register(Type)
-admin.site.register(Brand)
-admin.site.register(Country)
-admin.site.register(Region)
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'type',
+        'brand',
+        'country',
+        'region',
+        'flavour',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('name',)
+
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class RegionAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class FlavourAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Type, TypeAdmin)
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Country, CountryAdmin)
+admin.site.register(Region, RegionAdmin)
+admin.site.register(Flavour, FlavourAdmin)
