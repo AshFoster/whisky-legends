9676d70fffae2e854c35d26408a685ad79754c63
AshFoster <ashfoster87@gmail.com>
2022-04-06 16:48:42 +0000

    Add basic structure and styling to shop.html, inc some javascript

---
+++ shop/static/shop/js/shop.js
@@ -0,0 +1,43 @@
+// CREDIT 
+// Idea for this function came from:
+// https://stackoverflow.com/questions/53384366/how-to-get-the-longest-height-of-the-div-from-pure-javascript
+/* 
+Finds maximum height of all divs with a particular class name and sets all of their heights to the maximum height
+*/
+function setMaxHeight(className) {
+    let maxHeight = 0;
+
+    document.querySelectorAll(className).forEach(function (e) {
+        e.style.minHeight = null;
+    });
+
+    document.querySelectorAll(className).forEach(function (e) {
+        if (e.clientHeight > maxHeight) {
+            maxHeight = e.clientHeight;
+        }
+    });
+
+    document.querySelectorAll(className).forEach(function (e) {
+        e.style.minHeight = maxHeight + 'px';
+    });
+    console.log(maxHeight);
+}
+// END CREDIT
+
+/* 
+Once the DOM has finshed loading call necessary functions
+*/
+document.addEventListener('DOMContentLoaded', function () {
+
+    setMaxHeight('.product-card-title');
+
+});
+
+/* 
+Set 'resize' event listener to call setMaxHeight function on all elements with .product-card-title class
+*/
+window.addEventListener('resize', function () {
+
+    setMaxHeight('.product-card-title');
+
+}, false);
