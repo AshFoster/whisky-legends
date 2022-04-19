ea394cd6ecbfd0ee4035e7cf9af46bd13c44e9a0
AshFoster <ashfoster87@gmail.com>
2022-04-19 16:32:04 +0000

    Added cart context and delivery calculations

--- whiskylegends/settings.py
+++ whiskylegends/settings.py
@@ -80,6 +80,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'cart.contexts.cart_contents',
             ],
         },
     },
@@ -198,3 +199,6 @@ if 'USE_AWS' in os.environ:
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 45
+STANDARD_DELIVERY_PERCENTAGE = 10
