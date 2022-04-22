578591e1bb08bb3bd0bdc56cc40f7398f5ea6ad7
AshFoster <ashfoster87@gmail.com>
2022-04-22 15:58:42 +0000

    Add 'remove item' functionality to cart.html

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_cart(request):
@@ -29,13 +29,32 @@ def update_cart(request, product_id):
     A view to update the quantity of a specified product
     in the cart to a specified amount
     """
-    quantity = int(request.POST.get('quantity'))
-    cart = request.session.get('cart', {})
+    try:
+        quantity = int(request.POST.get('quantity'))
+        print(quantity)
+        cart = request.session.get('cart', {})
 
-    if quantity > 0:
-        cart[product_id] = quantity
-    else:
+        if quantity > 0:
+            cart[product_id] = quantity
+        else:
+            cart.pop(product_id)
+
+        request.session['cart'] = cart
+        # return redirect(reverse('view_cart'))
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
+
+
+def remove_from_cart(request, product_id):
+    """
+    A view to remove an item from the shopping cart
+    """
+    try:
+        cart = request.session.get('cart', {})
         cart.pop(product_id)
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
 
-    request.session['cart'] = cart
-    return redirect(reverse('view_cart'))
+    except Exception as e:
+        return HttpResponse(status=500)
