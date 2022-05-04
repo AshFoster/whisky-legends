9c83e339370e3e7adf3a70222ece5ee39d5719c8
AshFoster <ashfoster87@gmail.com>
2022-05-04 07:25:11 +0000

    Added previous_order view and updated checkout view so users can save and update their delivery info

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='profile')
+    path('', views.profile, name='profile'),
+    path('orders/', views.orders, name='orders'),
+    path('orders/<order_number>', views.previous_order, name='previous_order'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 
+from checkout.models import Order
+
 from .models import UserProfile
 from .forms import UserProfileForm
 
 
 def profile(request):
     """
-    A view to display the current user's profile.
+    A view to display the current user's profile info.
     """
     profile = get_object_or_404(UserProfile, user=request.user)
 
@@ -23,6 +25,41 @@ def profile(request):
     context = {
         'form': form,
         'viewing_profile': True
+    }
+
+    return render(request, template, context)
+
+
+def orders(request):
+    """
+    A view to display the current user's order history.
+    """
+    profile = get_object_or_404(UserProfile, user=request.user)
+    orders = profile.orders.all()
+
+    template = 'profiles/orders.html'
+    context = {
+        'orders': orders,
+    }
+
+    return render(request, template, context)
+
+
+def previous_order(request, order_number):
+    """
+    A view to display a previous order of the current user.
+    """
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
