266da17c1193370c53f22af17e5fa5492fb28ecc
AshFoster <ashfoster87@gmail.com>
2022-05-09 16:16:01 +0000

    Add 'edit products' functionality

--- shop/urls.py
+++ shop/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('delete_review/<review_id>/', views.delete_review,
          name='delete_review'),
     path('add_product/', views.add_product, name='add_product'),
+    path('edit_product/<int:product_id>/', views.edit_product,
+         name='edit_product'),
 ]
