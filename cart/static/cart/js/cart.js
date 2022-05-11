fc06108504affdaa044a9f10a153127187eb8c25
AshFoster <ashfoster87@gmail.com>
2022-05-11 15:25:28 +0000

    Move cart js into separate cart.js file

---
+++ cart/static/cart/js/cart.js
@@ -0,0 +1,47 @@
+// Update quantity and reload on click
+// CREDIT - https://stackoverflow.com/questions/64612746/how-would-i-do-this-ajax-jquery-in-vanilla-js
+document.querySelectorAll('.update-btn').forEach(item => {
+    item.addEventListener('click', function () {
+        let updateForm = this.closest('.update-form');
+        let qtyInput = updateForm.querySelector('.qty-input');
+
+        if (parseInt(qtyInput.value) >= parseInt(qtyInput.min) && parseInt(qtyInput.value) <= parseInt(qtyInput.max)) {
+            let formData = new FormData(updateForm);
+            let productId = qtyInput.getAttribute('id').split('id-qty-')[1];
+            let url = `/cart/update/${productId}/`;
+            let csrfToken = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
+
+            let httpRequest = new XMLHttpRequest();
+            httpRequest.open('POST', url);
+            httpRequest.setRequestHeader('X-CSRF-Token', csrfToken);
+            httpRequest.onreadystatechange = function () {
+                if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+                    location.reload();
+                }
+            };
+            httpRequest.send(formData);
+        } else {
+            updateForm.submit.click();
+        }
+    });
+});
+
+// Remove item and reload on click
+// CREDIT - https://stackoverflow.com/questions/64612746/how-would-i-do-this-ajax-jquery-in-vanilla-js
+document.querySelectorAll('.remove-btn').forEach(item => {
+    item.addEventListener('click', function () {
+        let productId = this.getAttribute('id').split('remove-')[1];
+        let url = `/cart/remove/${productId}/`;
+        let csrfToken = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
+        let params = 'csrfmiddlewaretoken=' + encodeURIComponent(csrfToken);
+        let httpRequest = new XMLHttpRequest();
+        httpRequest.open('POST', url);
+        httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        httpRequest.onreadystatechange = function () {
+            if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+                location.reload();
+            }
+        };
+        httpRequest.send(params);
+    });
+});
